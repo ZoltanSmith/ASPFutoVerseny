da2d5efa54bf618c6e992342c37b254ae67baac9
Zoltan Kovacs <157127975+ZoltanSmith@users.noreply.github.com>
2026-08-18 19:37:37 +0200

    Add project files.

---
+++ ASPFutoVerseny/Models/Versenyzo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASPFutoVerseny.Models;
+
+public partial class Versenyzo
+{
+    [Key]
+    public uint Id { get; set; }
+
+    [Required]
+    [StringLength(100, MinimumLength = 6)]
+    public string Nev { get; set; } = null!;
+
+    [MaxLength(45)]
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    [Range(10,100)]
+    public uint Kor { get; set; }
+
+    [Required]
+    [Range(100,1000)]
+    public float Tav { get; set; }
+
+    [Required]
+    public TimeOnly KvalifikaciosIdo { get; set; }
+
+    [Required]
+    public bool? Profi { get; set; }
+}
